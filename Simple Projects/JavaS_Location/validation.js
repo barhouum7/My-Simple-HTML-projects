28626c83ef443e211087f776ac95184146a0c6a7
barhouum7 <bb.med2000@gmail.com>
2019-10-03 18:35:52 +0100

    Some Static Websites Created By Me.

---
+++ Simple Projects/JavaS_Location/validation.js
@@ -0,0 +1,35 @@
+function A() {
+	var x = prompt("Write your PassWord please !" , "Name");
+	var name = "ibrahim";
+
+	if ( x == name.toUpperCase() || x == name.toLowerCase() )
+		{ 
+			window.location = "https://www.facebook.com/" ; }
+			else{
+			alert("Review your PassWord !!!");
+		}
+}
+
+function B() {
+	var x = prompt("Write your PassWord please !" , "Name");
+	var name = "ibrahim";
+
+	if ( x == name.toUpperCase() || x == name.toLowerCase() )
+		{ 
+			window.location = "https://www.google.com/" ; }
+			else{
+			alert("Review your PassWord !!!");
+		}
+}
+
+function C() {
+	var x = prompt("Write your PassWord please !" , "Name");
+	var name = "ibrahim";
+
+	if ( x == name.toUpperCase() || x == name.toLowerCase() )
+		{ 
+			window.location = "https://www.youtube.com/" ; }
+			else{
+			alert("Review your PassWord !!!");
+		}
+}
